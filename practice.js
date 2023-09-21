bdfee1c09be36a90e9cac184880d604cbd2cb982
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2023-09-21 21:42:06 +0300

    js pratice

---
+++ practice.js
@@ -0,0 +1,23 @@
+let theTitle="Elzero",
+theDescription="Elzero web school"
+,theDate="25/10";
+
+let card=`
+<div class="card">
+<div class="child">
+    <h3>${theTitle}</h3>
+    <p>${theDescription}</p>
+    <span>${theDate}</span>
+  </div> 
+</div>`
+
+
+document.write(card);
+document.write(card);
+document.write(card);
+document.write(card);
+
+
+
+
+
