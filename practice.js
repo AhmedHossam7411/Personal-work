212c719b4b3b777551fca1db73ce4d7378e9ffce
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2023-09-24 21:42:30 +0300

    practice

--- practice.js
+++ practice.js
@@ -1,121 +1,19 @@
-// let theTitle="Elzero",
-// theDescription="Elzero web school"
-// ,theDate="25/10";
-
-// let card=`
-// <div class="card">
-// <div class="child">
-//     <h3>${theTitle}</h3>
-//     <p>${theDescription}</p>
-//     <span>${theDate}</span>
-//   </div> 
-// </div>`
-
-
-// document.write(card);
-// document.write(card);
-// document.write(card);
-// document.write(card);
-
-
-
-// /*
-//   Switch Challenge
-// */
-
-// let job = "Manager";
-// let salary = 0;
-
-// // if (job === "Manager") {
-// //   salary = 8000;
-// // } else if (job === "IT" || job === "Support") {
-// //   salary = 6000;
-// // } else if (c) {
-// //   salary = 7000;
-// // } else {
-// //   salary = 4000;
-// // }
-
-// switch(job === "Manager"){
-//      default:
-//       salary=4000;
-//       console.log(salary);
-//       break;
-
-//    case job === "IT" || job === "Support":
-//     salary=6000;
-//     console.log(salary);
-//     break;
-
-//     case job==="Customer service" :
-//       salary=7000;
-//       console.log(salary);
-//       break;
-
-//       case job === "Manager" :
-//         salary=8000;
-//         console.log(salary);
-//         break;
-// }
-
-
-// console.log(salary);
-
-
-/*
-  Array Challenge
-*/
-
-let zero = 0;
-
-let counter = 3;
-
-let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
-
-// Write Code Here
-
-console.log(my); // ["Osama", "Elham", "Mazero", "Ahmed"];
-
-console.log(my.slice("????")); // ["Elham", "Mazero"]
-
-console.log(); // "Elzero"
-
-console.log(); // "rO"
+let name="Ahmed";
+document.write(`<div>
+Hello
+<p> Hi ${name}
+</p>
+</div>`);
+ 
+let list=[1,2,3,4,5];
 
 
 
 
+console.log(list);
+document.getElementById("btns").onclick=function move(){
+    console.log(`${name}`);
+};
 
-/*
-  If Challenge
-*/
 
-// let holidays = 0;
-// let money = 0;
 
-// switch (holidays) {
-//   case 0:
-//     money = 5000;
-//     console.log(`My Money is ${money}`);
-//     break;
-//   case 1:
-//   case 2:
-//     money = 3000;
-//     console.log(`My Money is ${money}`);
-//     break;
-//   case 3:
-//     money = 2000;
-//     console.log(`My Money is ${money}`);
-//     break;
-//   case 4:
-//     money = 1000;
-//     console.log(`My Money is ${money}`);
-//     break;
-//   case 5:
-//     money = 0;
-//     console.log(`My Money is ${money}`);
-//     break;
-//   default:
-//     money = 0;
-//     console.log(`My Money is ${money}`);
-// }
