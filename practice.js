42d35185c3fe86f2bbaaf97128488d3a9dfdb785
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2023-09-30 11:16:33 +0300

    practice on array functions

--- practice.js
+++ practice.js
@@ -1,13 +1,41 @@
-  
-function ahmed(car){
-    car="w"
-};
+    // window.print();
+    // const numbers = [45, 4, 9, 16, 25];
+     
+    // for(i=0;i<numbers.length;i++){
+    //     numbers[i]*=2;
+    // };
+    // console.log(numbers);
 
-function omar(){
-    let car=10;
-    console.log(car);
-};
+    const numbers = [45, 4, 9, 16, 25];
 
-omar();
+let txt = "";
+numbers.forEach(myFunction);
+document.getElementById("demo").innerHTML = txt;
 
-console.log(car);
+function myFunction(value, index, array) {
+  txt += value + "<br>"; 
+}
+    
+    
+    
+    
+
+// let x = 100 / "Apple";
+// let y=!isNaN(x)? true: false;
+// console.log(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
